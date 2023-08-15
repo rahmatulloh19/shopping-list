a1859d0e98a50fee5ed629a9bbc65855a9d6de9b
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2023-08-15 07:55:42 +0500

    do changes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-// Elements transferred from index.html
+// exporting elements from DOM
+const elForm = document.querySelector(".js-form");
+const elOrder = document.querySelector(".js-input_order");
+const elCount = document.querySelector(".js-input_count");
+const elRecordBtn = document.querySelector(".js-btn_record");
+const elResetBtn = document.querySelector(".js-btn_reset");
+const elList = document.querySelector(".js-list");
+const elOut = document.querySelector(".js-outging_text");
 
-let elForm = document.querySelector(".js-form");
-let elOrder = document.querySelector(".js-input_order");
-let elCount = document.querySelector(".js-input_count");
-let elList = document.querySelector(".js-list");
-
-// opened by default orders
+// by default orders
 let orders = [
   {
     user_order: "Buy Benanas",
@@ -15,56 +17,95 @@ let orders = [
     user_order: "Buy Apple",
     user_count: 11
   },
-  {
-    user_order: "Buy Favorite Book",
-    user_count: 9
-  },
-  {
-    user_order: "Pay Bill",
-    user_count: 2
-  },
-  {
-    user_order: "Damdaran Milk",
-    user_count: 10
-  },
 ]
 
-// Maked new function for arrays
-function renderItems(array) {
-
-  elList.innerHTML = ""
-
+// preparing render function
+function renderOrders(array) {
+  // before working the function we cleaning list's textContent for don't redraw
+  elList.innerHTML = "";
+  
   array.forEach(function(item) {
-    // Created elements and added classes
-    let liElement = document.createElement("li");
+    // making elements, adding class to thier classlist and giving textContent
+    const liElement = document.createElement("li");
     liElement.classList.add("item");
-
-    let spanElement = document.createElement("span");
-    spanElement.classList.add("item-count");
-
-    // TextContent gave to created elements
-    liElement.textContent = item.user_order;
-    spanElement.textContent = item.user_count;
+    liElement.textContent = item.user_order
     
-    // Connected the elements together
-    liElement.prepend(spanElement);
-    elList.append(liElement);
-  })
+    const countElement = document.createElement("span");
+    countElement.classList.add("item-count");
+    countElement.textContent = item.user_count;
+    
+    // chaining element
+    liElement.prepend(countElement);
+    elList.appendChild(liElement);
+  });
 }
 
-renderItems(orders);
+renderOrders(orders);
+
+
+const record = new webkitSpeechRecognition();
+
+elRecordBtn.addEventListener("click", function() {
+  record.start();
+  
+  record.onresult = function(evt) {
+    // getting recorded text
+    const recordedText = evt.results[0][0].transcript;
+    // converting text to array for iterate it
+    const convertRecordedText = recordedText.split(" ");
+    
+    // getting number and texts
+    let num;
+    let text = [];
+    convertRecordedText.forEach(function(item) {
+      // checking items
+      if(Number(item)) {
+        num = item;
+      } else {
+        text.push(item);
+      }
+    })
+    // collapse array
+    let convertString = text.join(" ");
+    
+    // change firstcharacter from anyone to upperCase 
+    convertString = convertString.replace(convertString.charAt(0), convertString.charAt(0).toUpperCase());
+    if(num !== "" && num !== undefined && convertString !== "") {
+      elCount.value = num;
+      elOrder.value = convertString;
+      elOut.textContent = "Succesfully added✅"
+    } else {
+      elOut.textContent = "Please say count with order"
+    }
+  }
+})
+
+// function for reset btn
+elResetBtn.addEventListener("click", function() {
+  elOrder.value = "";
+  elCount.value = "";
+  elOut.textContent = "";
+})
 
 elForm.addEventListener("submit", function(evt) {
   evt.preventDefault();
-
+  
   let elOrderValue = elOrder.value.trim();
   let elCountValue = elCount.value;
-
-  let new_order = {
-    user_order: elOrderValue,
-    user_count: elCountValue
+  
+  // checking values for don't push empty value
+  if(elOrderValue !== "" && elCountValue !== "") {
+    const newOrder = {
+      user_order: elOrderValue,
+      user_count: elCountValue
+    }
+    
+    orders.push(newOrder);
   }
 
-  orders.push(new_order);
-  renderItems(orders);
+  renderOrders(orders);
+  
+  elOrder.value = "";
+  elCount.value = "";
+  elOut.textContent = "";
 })
